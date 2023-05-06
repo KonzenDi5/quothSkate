7b9cdbac886b50bbd2fc113759a2d7c742dec1c9
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-05-06 16:29:46 -0300

    commitNewFooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import './estilos/estiloFooter.css'
+import mapa from './images/mapa.png'
+import logo from './images/quothwhite.png'
+import telefone from './images/telefone.png'
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; 2023 - Meu Site</p>
-    </footer>
+    <div className='containerFooter'>
+      <div className='logoFooter'>
+        <img className='logoQuoth' alt='mapa' src={logo}/>
+      </div>
+      <div className='contato>'>
+        <img className='telefone' alt='mapa' src={telefone}/>
+        <p>quothskateshop@contato.com</p>
+        <p>(24) 99438-0738</p>
+        
+      </div>
+      <div className='localizacao'>
+        <img className='mapa' alt='mapa' src={mapa}/>
+        <p>Rua Tocantins - 646, Rio de Janeiro, Brasil</p>
+      </div>
+    </div>
   );
 }
 
