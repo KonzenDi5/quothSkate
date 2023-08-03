12531a534f5fd9d21784f47769beb4b5a5346703
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-08-03 10:52:32 -0300

    commit

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,4 +1,3 @@
-// Cart.js
 import React, { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../../components/CartContext';
 import './Carrinho.css';
@@ -8,60 +7,66 @@ function Cart() {
   const [itemQuantities, setItemQuantities] = useState({});
 
   useEffect(() => {
-    // Função para calcular a quantidade total de cada item no carrinho
     const calculateItemQuantities = () => {
-    const quantities = {};
-    cartItems.forEach(item => {
-      if (item.id in quantities) {
-        quantities[item.id] += 1;
-      } else {
-        quantities[item.id] = 1;
-      }
-    });
-    return quantities;
-  };
+      const quantities = {};
+      cartItems.forEach(item => {
+        if (item.id in quantities) {
+          quantities[item.id] += 1;
+        } else {
+          quantities[item.id] = 1;
+        }
+      });
+      return quantities;
+    };
 
-  setItemQuantities(calculateItemQuantities());
-}, [cartItems]);
-  // Função para calcular o total da compra
+    setItemQuantities(calculateItemQuantities());
+  }, [cartItems]);
+
   const calculateTotal = () => {
     return cartItems.reduce((total, item) => total + parseFloat(item.price.replace('R$ ', '')), 0);
   };
 
   return (
     <div className="cart-container">
-      <div className="cart-items">
-        {/* Renderize os itens do carrinho com bolinha vermelha mostrando a quantidade */}
-        {Object.keys(itemQuantities).map(itemId => {
-          const item = cartItems.find(item => item.id === Number(itemId));
+      <table className="cart-table">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Quantidade</th>
+            <th>Valor da Entrega</th>
+            <th>Valor do Produto</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(itemQuantities).map(itemId => {
+            const item = cartItems.find(item => item.id === Number(itemId));
 
-          // Adicione a verificação para garantir que os dados do item existam
-          if (!item) {
-            return null;
-          }
+            if (!item) {
+              return null;
+            }
 
-          return (
-            <div key={itemId} className="cart-item">
-              <img src={item.image} alt={item.name} />
-              <div>{item.name}</div>
-              <div>{item.price}</div>
-              {/* Exibir a bolinha vermelha com a quantidade */}
-              {itemQuantities[itemId] > 0 && (
-                <div className="item-quantity">{itemQuantities[itemId]}</div>
-              )}
-              <button onClick={() => addToCart(item)}>Adicionar</button>
-              {/* Adicionar verificação para evitar erro ao remover item que não está no carrinho */}
-              {itemQuantities[itemId] > 0 && (
-                <button onClick={() => removeFromCart(item.id)}>Remover</button>
-              )}
-            </div>
-          );
-        })}
-      </div>
-      {/* Mostrar o total da compra */}
+            return (
+              <tr className='itens' key={itemId}>
+                <td>
+                  <img className='imagem-item
+                  ' src={item.image} alt={item.name} />
+                  {item.name}
+                </td>
+                <td className="quantity-buttons">
+                  <button onClick={() => removeFromCart(item.id)}>-</button>
+                  <span>{itemQuantities[itemId]}</span>
+                  <button onClick={() => addToCart(item)}>+</button>
+                </td>
+                <td>{item.deliveryPrice}</td>
+                <td>{item.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <div className='comprar'>
-      <div className="cart-total">Total: R$ {calculateTotal()}</div>
-      <div className='button-comprar'>COMPRAR</div>
+        <div className="cart-total">Total: R$ {calculateTotal()}</div>
+        <div className='button-comprar'>COMPRAR</div>
       </div>
     </div>
   );
